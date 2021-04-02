ab270d179f82f70602cef49af2aae685b7a47a1a
Chris Gianelle <cgianelle@cipherhealth.com>
2021-04-02 17:19:00 -0400

    changes for twilio (#1)
    * changes for twilio

--- pull_request.py
+++ pull_request.py
@@ -1,5 +1,48 @@
 from github import Github
 import yaml
+from twilio.rest import Client
+
+class TwilioUtils:
+    def __init__(self, sid, token):
+        self.acct_sid = sid
+        self.acct_token = token
+        self._to_phone_number = None
+        self._from_phone_number = None
+
+    @property
+    def to_phone_number(self):
+        return self._to_phone_number
+
+    @to_phone_number.setter
+    def to_phone_number(self, phone_number):
+        self._to_phone_number = phone_number
+
+    @to_phone_number.deleter
+    def to_phone_number(self):
+        del self._to_phone_number
+
+    @property
+    def from_phone_number(self):
+        return self._from_phone_number
+
+    @from_phone_number.setter
+    def from_phone_number(self, phone_number):
+        self._from_phone_number = phone_number
+
+    @from_phone_number.deleter
+    def from_phone_number(self):
+        del self._from_phone_number
+
+    def connectToTwilio(self):
+        self.twilio_client = Client(self.acct_sid, self.acct_token)
+
+    def sendSMS(self, message):
+        return self.twilio_client.messages.create(
+                     body=message,
+                     from_=self._from_phone_number,
+                     to=self._to_phone_number
+                 )
+
 
 class GithubUtils:
     def __init__(self, personal_access_token, base_url=None):
@@ -13,28 +56,55 @@ class GithubUtils:
             self.github = Github(base_url=self.base_url, login_or_token=self.personal_access_token)
 
     def findReviewer(self, repo, user):
-        repo = self.github.get_repo(repo)
-        pulls = repo.get_pulls(state='open', sort='created')
+        r = self.github.get_repo(repo)
+        pulls = r.get_pulls(state='open', sort='created')
         pull_request_titles = []
         for pr in pulls:
-            requests = pr.get_review_requests()
-            for request in requests:
-                for r in request:
-                    if r.login == user:
-                        pull_request_titles.append(pr.title)
+            # todo: need to check the assignees array
+            if self.__isUserAmongAssignees(user, pr.assignees):
+                pull_request_titles.append(self.__createMesssageTitle(repo,pr))
+            else:
+                requests, _ = pr.get_review_requests()
+                if self.__isUserAmongReviewers(user, requests):
+                    pull_request_titles.append(self.__createMesssageTitle(repo,pr))
         return pull_request_titles
-            
+
+    def __isUserAmongAssignees(self, user, assignees):
+        for assignee in assignees:
+            if assignee and assignee.login == user:
+                return True
+        return False
+
+    def __isUserAmongReviewers(self, user, reviewers):
+        for reviewer in reviewers:
+            if reviewer.login == user:
+                return True
+        return False
+
+    def __createMesssageTitle(self, repo, pull_request):
+        return repo+' PR #'+str(pull_request.number)+": "+pull_request.title
+
+
+
+     
 if __name__ == '__main__':
     stream = open("config.yml", 'r')
     dictionary = yaml.safe_load(stream)
     try:
         github_utils = GithubUtils(dictionary['github']['personal_access_token'])
+        twilio_utils = TwilioUtils(dictionary['twilio']['account_sid'], dictionary['twilio']['auth_token'])
         github_utils.connectToGithub()
+        twilio_utils.from_phone_number = dictionary['twilio']['from_phone_number']
+        twilio_utils.to_phone_number = dictionary['twilio']['to_phone_number']
+        twilio_utils.connectToTwilio()
+
         for repo in dictionary['github']['repos']:
             print("Checking repo",repo)
             pulls = github_utils.findReviewer(repo, dictionary['github']['login'])
-            print(pulls)
+            for pull in pulls:
+                print("sending {0}".format(pull))
+                twilio_utils.sendSMS('You have a pending review for:\n'+pull)
 
-    except:
-        print("Check your configuration file and make sure that you have configured your personal_access_token under github")
+    except Exception as e:
+        print(e)
         
