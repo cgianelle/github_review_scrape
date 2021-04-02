fa65378eaac682644ef866c2775ee657edb0e33f
Chris Gianelle <cgianelle@cipherhealth.com>
2021-04-02 15:44:34 -0400

    changes for twilio

--- pull_request.py
+++ pull_request.py
@@ -1,5 +1,48 @@
 from github import Github
 import yaml
+from twilio.rest import Client
+
+class TwilioUtils:
+    def __init__(self, sid, token):
+        self.acct_sid = sid
+        self.acct_token = token
+        self._to_phone_number = None
+        self._from_phone_number = None
+
+    @property
+    def to_phone_number(self):
+        return self._to_phone_number
+
+    @to_phone_number.setter
+    def to_phone_number(self, phone_number):
+        self._to_phone_number = phone_number
+
+    @to_phone_number.deleter
+    def to_phone_number(self):
+        del self._to_phone_number
+
+    @property
+    def from_phone_number(self):
+        return self._from_phone_number
+
+    @from_phone_number.setter
+    def from_phone_number(self, phone_number):
+        self._from_phone_number = phone_number
+
+    @from_phone_number.deleter
+    def from_phone_number(self):
+        del self._from_phone_number
+
+    def connectToTwilio(self):
+        self.twilio_client = Client(self.acct_sid, self.acct_token)
+
+    def sendSMS(self, message):
+        return self.twilio_client.messages.create(
+                     body=message,
+                     from_=self._from_phone_number,
+                     to=self._to_phone_number
+                 )
+
 
 class GithubUtils:
     def __init__(self, personal_access_token, base_url=None):
@@ -23,18 +66,26 @@ class GithubUtils:
                     if r.login == user:
                         pull_request_titles.append(pr.title)
         return pull_request_titles
-            
+     
 if __name__ == '__main__':
     stream = open("config.yml", 'r')
     dictionary = yaml.safe_load(stream)
     try:
         github_utils = GithubUtils(dictionary['github']['personal_access_token'])
+        twilio_utils = TwilioUtils(dictionary['twilio']['account_sid'], dictionary['twilio']['auth_token'])
         github_utils.connectToGithub()
+        twilio_utils.from_phone_number = dictionary['twilio']['from_phone_number']
+        twilio_utils.to_phone_number = dictionary['twilio']['to_phone_number']
+        twilio_utils.connectToTwilio()
+
         for repo in dictionary['github']['repos']:
             print("Checking repo",repo)
             pulls = github_utils.findReviewer(repo, dictionary['github']['login'])
-            print(pulls)
+            for pull in pulls:
+                print("sending {0}".format(pull))
+                twilio_utils.sendSMS(pull)
 
-    except:
+    except Exception as e:
+        print(e)
         print("Check your configuration file and make sure that you have configured your personal_access_token under github")
         
